dbb60a59983743cfe9423ce694b9231d0f3bd0f3
MypkaXD <nicklayschannel@yandex.ru>
2026-05-26 08:08:40 +0300

    added output folder

--- script.sh
+++ script.sh
@@ -6,6 +6,6 @@ pip install --no-cache-dir submodules/diff-gaussian-rasterization submodules/sim
 mkdir output
 
 xvfb-run -a python3 convert.py -s /app/chair
-python3 train.py -s /app/chair --iterations 1000 --eval -m output/
-python3 render.py -m /app/gaussian-splatting/output
-python3 metrics.py -m output/
+python3 train.py -s /app/chair --iterations 1000 --eval -m /app/output/
+python3 render.py -m /app/output
+python3 metrics.py -m /app/output/
